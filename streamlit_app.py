5ddbae252ff3e84a9e7f4215c1e93fac005f6203
funasako <157328833+funasako@users.noreply.github.com>
2024-11-28 11:47:26 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,7 @@ def convert_df_to_excel(df):
             'max': df['X'].max(),  # 横軸の最大値をXデータの最大値に設定
             'reverse': False,
             'label': 'Wavelength / nm',  # 横軸ラベルを設定
+            'font': {'color': 'black', 'size': 16, 'name': 'Arial'},  # フォント設定
         })
 
         # 縦軸設定
@@ -59,10 +60,11 @@ def convert_df_to_excel(df):
             'major_tick_mark': 'inside',
             'label': 'Absorbance (arb units.)',  # 縦軸ラベルを設定
             'major_gridlines': {'visible': False},  # 縦軸の目盛線を削除
+            'font': {'color': 'black', 'size': 16, 'name': 'Arial'},  # フォント設定
         })
 
-        # フォント設定
-        chart.set_chartstyle({'font': {'color': 'black', 'size': 16, 'name': 'Arial'}})
+        # フォント設定（グラフ内すべての文字）
+        chart.set_title({'name': 'Absorbance vs Wavelength', 'name_font': {'color': 'black', 'size': 16, 'name': 'Arial'}})
 
         # グラフを配置
         worksheet.insert_chart('A3', chart)
