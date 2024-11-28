b9b67c871a66fd1f91b6f66fe852adcb6fa76817
funasako <157328833+funasako@users.noreply.github.com>
2024-11-28 14:00:13 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,33 @@ def convert_files_to_excel(files):
         cell_format = workbook.add_format({'font_name': 'Times New Roman', 'font_size': 12})
         border_format = workbook.add_format({'font_name': 'Times New Roman', 'font_size': 12, 'border': 1})
 
+        # グラフの初期設定
+        chart = workbook.add_chart({'type': 'scatter', 'subtype': 'smooth'})
+        chart.set_size({'width': 533, 'height': 377})
+        chart.set_chartarea({'border': {'none': True}, 'fill': {'none': True}})
+        chart.set_plotarea({'border': {'color': 'black', 'width': 1.5}, 'fill': {'none': True}})
+        chart.set_legend({'none': True})
+        chart.set_x_axis({
+            'line': {'color': 'black', 'width': 1.5},
+            'major_tick_mark': 'inside',
+            'major_unit': 100,
+            'min': 300,
+            'max': df['X'].max(),
+            'reverse': False,
+            'name': 'Wavelength / nm',
+            'num_font': {'color': 'black', 'size': 16, 'name': 'Arial'},
+            'name_font': {'color': 'black', 'size': 16, 'name': 'Arial', 'bold': False},
+        })
+        chart.set_y_axis({
+            'line': {'color': 'black', 'width': 1.5},
+            'major_tick_mark': 'inside',
+            'name': 'Absorbance',
+            'major_gridlines': {'visible': False},
+            'num_font': {'color': 'black', 'size': 16, 'name': 'Arial'},
+            'name_font': {'color': 'black', 'size': 16, 'name': 'Arial', 'bold': False},
+            'major_unit': 0.1,
+        })
+        
         start_col = 11  # 初期列（L列 = インデックス11）
 
         for file in files:
@@ -56,37 +83,14 @@ def convert_files_to_excel(files):
                 worksheet.write_formula(i + 2, start_col + 2, f"={chr(65 + start_col + 1)}{i+3}*${chr(65 + start_col + 2)}$1+${chr(65 + start_col + 2)}$2", cell_format)
 
             # グラフを作成
-            chart = workbook.add_chart({'type': 'scatter', 'subtype': 'smooth'})
+
             chart.add_series({
                 'categories': f"=Data!${chr(65 + start_col)}$3:${chr(65 + start_col)}${len(df) + 2}",
                 'values': f"=Data!${chr(65 + start_col + 2)}$3:${chr(65 + start_col + 2)}${len(df) + 2}",
                 'marker': {'type': 'none'},
                 'line': {'color': '#008EC0', 'width': 1.5},
             })
-            chart.set_size({'width': 533, 'height': 377})
-            chart.set_chartarea({'border': {'none': True}, 'fill': {'none': True}})
-            chart.set_plotarea({'border': {'color': 'black', 'width': 1.5}, 'fill': {'none': True}})
-            chart.set_legend({'none': True})
-            chart.set_x_axis({
-                'line': {'color': 'black', 'width': 1.5},
-                'major_tick_mark': 'inside',
-                'major_unit': 100,
-                'min': 300,
-                'max': df['X'].max(),
-                'reverse': False,
-                'name': 'Wavelength / nm',
-                'num_font': {'color': 'black', 'size': 16, 'name': 'Arial'},
-                'name_font': {'color': 'black', 'size': 16, 'name': 'Arial', 'bold': False},
-            })
-            chart.set_y_axis({
-                'line': {'color': 'black', 'width': 1.5},
-                'major_tick_mark': 'inside',
-                'name': 'Absorbance',
-                'major_gridlines': {'visible': False},
-                'num_font': {'color': 'black', 'size': 16, 'name': 'Arial'},
-                'name_font': {'color': 'black', 'size': 16, 'name': 'Arial', 'bold': False},
-                'major_unit': 0.1,
-            })
+
             start_col += 4  # 次のファイルは右に4列ずらして書き込み
             
         worksheet.insert_chart("A3", chart)
