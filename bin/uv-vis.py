314c0e47c5f7ba4936606354c5e0ad3a460df7fc
funasako <157328833+funasako@users.noreply.github.com>
2024-11-30 13:09:57 +0900

    Update uv-vis.py

--- bin/uv-vis.py
+++ bin/uv-vis.py
@@ -8,8 +8,8 @@ import pytz
 
 
 # タイトル等
-st.set_page_config(page_title="JASCO UV-vis Spectra Formatter")
-st.title("JASCO UV-vis Spectra Formatter")
+st.set_page_config(page_title="UV-vis | JASCO Spectra Formatter")
+st.title("UV-vis | JASCO Spectra Formatter")
 st.write("※動作にはインターネット接続が必要です。")
 st.write("1. 装置が書き出したテキスト形式ファイルを用意する、もしくは、スペクトルマネージャーでテキストファイルをエクスポートする（ファイル名をしっかりつけておく）")
 st.write("2. 以下にドラッグ&ドロップしてグラフ表示。複数ファイルからプロット重ね書きも可能")
