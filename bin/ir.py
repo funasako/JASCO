4fbd6b0ec86e67718d0f0df4279a3fc706413f98
funasako <157328833+funasako@users.noreply.github.com>
2024-12-01 12:48:13 +0900

    Update ir.py

--- bin/ir.py
+++ bin/ir.py
@@ -179,7 +179,8 @@ def convert_files_to_excel(files):
         chart.set_y_axis({
             'line': {'color': 'black', 'width': 1.5},
             'major_tick_mark': 'inside',
-            'max':(num_files - 1) * 40 + 110,
+            'max': (num_files - 1) * 40 + 110,
+            'crossing': -1000,
             'name': 'Transmittance (%)',
             'major_gridlines': {'visible': False},
             'num_font': {'color': 'black', 'size': 16, 'name': 'Arial'},
